72db6c9836c67148f8e9ab18b0d89e6641ab9c1a
Na Rim <skfla07@naver.com>
2022-11-07 00:11:42 +0900

    [feat] navigation 및 홈화면 UI

---
+++ custom-house-front/components/TabNav.js
@@ -0,0 +1,56 @@
+import Home from "./home/Home";
+import RoutinList from "./routinList/RoutinList";
+import TopNav from "./routinList/TopNav";
+import React from "react";
+import "react-native-gesture-handler";
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+export default function TabNav() {
+  const Tab = createBottomTabNavigator();
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      tabBarOptions={{
+        tabBarStyle: {
+          backgroundColor: "#66CC99",
+        },
+      }}
+    >
+      <Tab.Screen
+        options={{
+          headerShown: false,
+          animationEnabled: false,
+          tabBarActiveTinitColor: "black",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              style={{ color: focused ? "#00B386" : "#404040" }}
+              name="ios-home"
+              size={25}
+            />
+          ),
+        }}
+        name="Home"
+        component={Home}
+      />
+      <Tab.Screen
+        name="Routine"
+        options={{
+          title: "ROUTINE RECOMMENDATION",
+          headerTitleStyle: {
+            fontWeight: "500",
+            fontSize: 15,
+          },
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              style={{ color: focused ? "#00B386" : "#404040" }}
+              name="ios-document-text-outline"
+              size={25}
+            />
+          ),
+        }}
+        component={TopNav}
+      />
+    </Tab.Navigator>
+  );
+}
