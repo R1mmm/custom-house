4684d8e3da25f06c884acaacfbba7475e6a65761
Yang Kyung Hun <ygh1254@hanyang.ac.kr>
2022-11-11 17:31:14 +0900

    [feat] Find Id, Find PW

--- custom-house-backend/src/main/java/Custom_HOUSE/CH/Controller/MemberController.java
+++ custom-house-backend/src/main/java/Custom_HOUSE/CH/Controller/MemberController.java
@@ -1,13 +1,17 @@
 package Custom_HOUSE.CH.Controller;
 
 import Custom_HOUSE.CH.Repository.MemberRepository;
+import Custom_HOUSE.CH.Repository.UserRepository;
 import Custom_HOUSE.CH.model.Member;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -15,7 +19,9 @@ public class MemberController {
 
     @Autowired
     private MemberRepository repository;
-    private String name;
+
+    @Autowired
+    private UserRepository urepository;
 
     @PostMapping("/api/new-member")
     public String saveMember(@RequestBody Member member) {
@@ -39,5 +45,16 @@ public class MemberController {
         return "Deleted member with id: " + id;
     }
 
+    @PostMapping("api/id")
+    public String findId(@RequestParam("name") String name,
+                               @RequestParam("birth") String birth){
+        return urepository.findByNameAndBirth(name, birth).getId();
+    }
+
+    @PostMapping("api/pw")
+    public String findPw(@RequestParam("id") String id,
+                               @RequestParam("name") String name){
+        return urepository.findByIdAndName(id, name).getPassword();
+    }
 }
 
--- custom-house-backend/src/main/java/Custom_HOUSE/CH/Repository/MemberRepository.java
+++ custom-house-backend/src/main/java/Custom_HOUSE/CH/Repository/MemberRepository.java
@@ -2,7 +2,11 @@ package Custom_HOUSE.CH.Repository;
 
 import Custom_HOUSE.CH.model.Member;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 public interface MemberRepository extends MongoRepository<Member, Integer> {
 
 }
+
