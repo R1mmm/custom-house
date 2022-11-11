d771a5d599e535877d4f9b6efdcc321ae36f2127
Na Rim <skfla07@naver.com>
2022-11-11 14:31:20 +0900

    [feat] 커스텀 루틴 추가 페이지 구현중

--- custom-house-front/components/customRoutine/CustomRoutine.jsx
+++ custom-house-front/components/customRoutine/CustomRoutine.jsx
@@ -1,14 +1,30 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function CustomRoutine() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>가전 제품 선택</Text>
       <View style={styles.productContainer}>
-        <View style={styles.iconBox}></View>
-        <View style={styles.iconBox}></View>
-        <View style={styles.iconBox}></View>
+        <View style={styles.iconBox}>
+          <Ionicons
+            name="ios-tv-sharp"
+            size={35}
+          />
+        </View>
+        <View style={styles.iconBox}>
+          <Ionicons
+            name="ios-water-sharp"
+            size={35}
+          />
+        </View>
+        <View style={styles.iconBox}>
+          <Ionicons
+            name="ios-shirt-sharp"
+            size={35}
+          />
+        </View>
         <View style={styles.iconBox}></View>
         <View style={styles.iconBox}></View>
         <View style={styles.iconBox}></View>
@@ -41,6 +57,9 @@ const styles = StyleSheet.create({
     borderRadius: "100%",
     height: 80,
     width: 80,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: "#EAEAEA",
     marginBottom: 10,
     marginTop: 10,
