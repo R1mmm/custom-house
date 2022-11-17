717821eb735c95848bb361991e5c38ae9ebcb697
Na Rim <skfla07@naver.com>
2022-11-18 00:09:52 +0900

    [etc] 로컬 폴더 경로 변경

--- custom-house-backend/src/main/java/Custom_HOUSE/CH/Repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package Custom_HOUSE.CH.Repository;
-
-import Custom_HOUSE.CH.model.Member;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MemberRepository extends MongoRepository<Member, Integer> {
-
-}
